6152051f5c925e41e154e61b8dd65bbe3fbada00
fengyx1024 <fengyx1024@163.com>
2022-05-31 23:02:36 +0800

    配置redis及测试-fengyx

--- src/main/java/com/shangma/Num3projectApplication.java
+++ src/main/java/com/shangma/Num3projectApplication.java
@@ -1,9 +1,11 @@
 package com.shangma;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.shangma.mapper")
 public class Num3projectApplication {
 
     public static void main(String[] args) {
