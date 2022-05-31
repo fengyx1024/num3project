acc972a525a2f43428b0cf0a2b45baa5eca4e562
fengyx1024 <fengyx1024@163.com>
2022-05-31 19:50:10 +0800

    Initial commit

---
+++ src/main/java/com/shangma/num3project/Num3projectApplication.java
@@ -0,0 +1,13 @@
+package com.shangma.num3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Num3projectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Num3projectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shangma/num3project/Num3projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shangma.num3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Num3projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
